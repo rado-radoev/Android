77bddc896bd23b1f85d5bbd416bfe3d2923ab127
Radoslav Radoev <rradoev@gmail.com>
2018-06-07 23:25:56 -0700

    very good bun unnescesseraly complicated idea. scratch that

--- Connect3/app/src/main/java/com/superlamer/connect3/MainActivity.java
+++ Connect3/app/src/main/java/com/superlamer/connect3/MainActivity.java
@@ -2,7 +2,6 @@ package com.superlamer.connect3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.menu.MenuView;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,21 +13,29 @@ public class MainActivity extends AppCompatActivity {
     // 0 = yellow; 1 = red;
 
     private int activePlayer = 0;
-    private int[][] gameState = new int[3][];
+    private static int[][] gameState = new int[3][];
+    private int[][]winningPositions = {
+            {0,1,2},
+            {3,4,5},
+            {6,7,8},
+            {1,4,7},
+            {2,5,8},
+            {0,4,8},
+            {2,4,6}
+    };
 
-    private void initializeGameState() {
+    public MainActivity() {
+        initializeGameState();
+    }
+
+    private static void initializeGameState() {
         for (int i = 0; i < gameState.length; i++) {
-            gameState[i] = new int[] {0,0,0};
+            gameState[i] = new int[] {2,2,2};
         }
     }
 
-    private void occupyIndex(int[] colRowToOccupy) {
-        int col = colRowToOccupy[0];
-        int row = colRowToOccupy[1];
-
-        if (gameState[row][col] == 0) {
-            gameState[row][col] = 1;
-        }
+    private void occupyCell(int col, int row) {
+        gameState[col][row] = activePlayer;
     }
 
     private boolean isCellOccupied(int col, int row) {
@@ -41,19 +48,21 @@ public class MainActivity extends AppCompatActivity {
         return  occupied;
     }
 
-    private boolean placeAnItem(ImageView item) {
+    private boolean placeItem(ImageView item) {
         boolean successful = false;
-        int[] colRow = getSelectedIndex(item.getTag().toString());
+        int[] colRow = getItemLocation(item.getTag().toString());
+        int col = colRow[0];
+        int row = colRow[1];
 
-        if (!isCellOccupied(colRow[0],colRow[1])) {
-            occupyIndex(colRow);
+        if (!isCellOccupied(col,row)) {
+            occupyCell(col, row);
             successful = true;
         }
 
         return successful;
     }
 
-    private int[] getSelectedIndex(String tag) {
+    private int[] getItemLocation(String tag) {
 
         int[] colRow = new int[2];
 
@@ -73,25 +82,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
-        initializeGameState();
 
-        counter.setTranslationY(-1000f);
+        int[] selectedIndex = getItemLocation(counter.getTag().toString());
 
-        if (activePlayer == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            if (placeAnItem(counter)) {
-                activePlayer = 1;
-                counter.animate().translationYBy(1000f).rotation(180).setDuration(2000);
+        if (!(isCellOccupied(selectedIndex[0], selectedIndex[1]))) {
+            counter.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                if (placeItem(counter)) {
+                    activePlayer = 1;
+                    counter.animate().translationYBy(1000f).rotation(180).setDuration(2000);
+                }
+            }
+            else {
+                counter.setImageResource(R.drawable.red);
+                if (placeItem(counter)) {
+                    activePlayer = 0;
+                    counter.animate().translationYBy(1000f).rotation(180).setDuration(2000);
+                }
             }
         }
-        else {
-            counter.setImageResource(R.drawable.red);
-            placeAnItem(counter);
-            if (placeAnItem(counter)) {
-                activePlayer = 0;
-                counter.animate().translationYBy(1000f).rotation(180).setDuration(2000);
-            }
+
+        for (int[] winningPosition : winningPositions) {
+
         }
+
 
 
     }
