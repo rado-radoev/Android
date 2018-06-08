d294f4095990907d5981810a4c0615d9229f42fd
Radoslav Radoev <rradoev@gmail.com>
2018-06-08 00:50:29 -0700

    app is working but the reset button is crashing the app

--- Connect3/app/src/main/java/com/superlamer/connect3/MainActivity.java
+++ Connect3/app/src/main/java/com/superlamer/connect3/MainActivity.java
@@ -1,117 +1,143 @@
 package com.superlamer.connect3;
 
+import android.media.ResourceBusyException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 0 = yellow; 1 = red;
+    // 0 = yellow, 1 = red
 
-    private int activePlayer = 0;
-    private static int[][] gameState = new int[3][];
-    private int[][]winningPositions = {
-            {0,1,2},
-            {3,4,5},
-            {6,7,8},
-            {1,4,7},
-            {2,5,8},
-            {0,4,8},
-            {2,4,6}
-    };
+    int activePlayer = 0;
 
-    public MainActivity() {
-        initializeGameState();
-    }
+    boolean gameIsActive = true;
 
-    private static void initializeGameState() {
-        for (int i = 0; i < gameState.length; i++) {
-            gameState[i] = new int[] {2,2,2};
-        }
-    }
+    // 2 means unplayed
 
-    private void occupyCell(int col, int row) {
-        gameState[col][row] = activePlayer;
-    }
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
-    private boolean isCellOccupied(int col, int row) {
-        boolean occupied = false;
-
-        if (gameState[col][row] == 1) {
-            occupied = true;
-        }
-
-        return  occupied;
-    }
-
-    private boolean placeItem(ImageView item) {
-        boolean successful = false;
-        int[] colRow = getItemLocation(item.getTag().toString());
-        int col = colRow[0];
-        int row = colRow[1];
-
-        if (!isCellOccupied(col,row)) {
-            occupyCell(col, row);
-            successful = true;
-        }
-
-        return successful;
-    }
-
-    private int[] getItemLocation(String tag) {
-
-        int[] colRow = new int[2];
-
-        String regexPattern = "\\d";
-        Pattern pattern = Pattern.compile(regexPattern);
-        Matcher matcher = pattern.matcher(tag);
-
-        int count = 0;
-        while (matcher.find()) {
-            count++;
-            colRow[count - 1] = Integer.valueOf(tag.substring(matcher.start(),matcher.end()));
-            tag.substring(matcher.start(),matcher.end());
-        }
-
-        return colRow;
-    }
+    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     public void dropIn(View view) {
+
         ImageView counter = (ImageView) view;
 
-        int[] selectedIndex = getItemLocation(counter.getTag().toString());
 
-        if (!(isCellOccupied(selectedIndex[0], selectedIndex[1]))) {
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
+
+            gameState[tappedCounter] = activePlayer;
+
             counter.setTranslationY(-1000f);
 
             if (activePlayer == 0) {
+
                 counter.setImageResource(R.drawable.yellow);
-                if (placeItem(counter)) {
-                    activePlayer = 1;
-                    counter.animate().translationYBy(1000f).rotation(180).setDuration(2000);
-                }
-            }
-            else {
+
+                activePlayer = 1;
+
+            } else {
+
                 counter.setImageResource(R.drawable.red);
-                if (placeItem(counter)) {
-                    activePlayer = 0;
-                    counter.animate().translationYBy(1000f).rotation(180).setDuration(2000);
+
+                activePlayer = 0;
+
+            }
+
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+
+                    // Someone has won!
+
+                    gameIsActive = false;
+
+                    String winner = "Red";
+
+                    if (gameState[winningPosition[0]] == 0) {
+
+                        winner = "Yellow";
+
+                    }
+
+                    TextView winnerMessage = findViewById(R.id.winnerMessage);
+
+                    winnerMessage.setText(winner + " has won!");
+
+                    LinearLayout layout = findViewById(R.id.playAgainLayout);
+
+                    layout.setVisibility(View.VISIBLE);
+
+                } else {
+
+                    boolean gameIsOver = true;
+
+                    for (int counterState : gameState) {
+
+                        if (counterState == 2) gameIsOver = false;
+
+                    }
+
+                    if (gameIsOver) {
+
+                        TextView winnerMessage =  findViewById(R.id.winnerMessage);
+
+                        winnerMessage.setText("It's a draw");
+
+                        LinearLayout layout = findViewById(R.id.playAgainLayout);
+
+                        layout.setVisibility(View.VISIBLE);
+
+                    }
+
                 }
+
             }
         }
-
-        for (int[] winningPosition : winningPositions) {
-
-        }
-
 
 
     }
 
+    public void playAgain(View view) {
+
+        gameIsActive = true;
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+
+        layout.setVisibility(View.INVISIBLE);
+
+        activePlayer = 0;
+
+        for (int i = 0; i < gameState.length; i++) {
+
+            gameState[i] = 2;
+
+        }
+
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+
+        for (int i = 0; i< gridLayout.getChildCount(); i++) {
+
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+
+        }
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
