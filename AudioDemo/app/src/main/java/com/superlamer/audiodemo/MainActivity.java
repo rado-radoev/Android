03e67d01b5a21addc9fb97dbcdad7dd3b37e9d8e
Radoslav Radoev <rradoev@gmail.com>
2018-06-08 23:57:51 -0700

    Audio Demo

--- AudioDemo/app/src/main/java/com/superlamer/audiodemo/MainActivity.java
+++ AudioDemo/app/src/main/java/com/superlamer/audiodemo/MainActivity.java
@@ -2,12 +2,25 @@ package com.superlamer.audiodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.motorcycle);
+    }
+
+    public void playAudio(View view) {
+        mediaPlayer.start();
+    }
+
+    public void pauseAudio(View view) {
+        mediaPlayer.pause();
     }
 }
