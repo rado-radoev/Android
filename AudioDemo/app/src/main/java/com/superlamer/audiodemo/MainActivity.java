11a41b1468bc98e6dc8733cca0aa827991a1f471
Radoslav Radoev <rradoev@gmail.com>
2018-06-09 00:41:48 -0700

    Audio Demo

--- AudioDemo/app/src/main/java/com/superlamer/audiodemo/MainActivity.java
+++ AudioDemo/app/src/main/java/com/superlamer/audiodemo/MainActivity.java
@@ -1,19 +1,59 @@
 package com.superlamer.audiodemo;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView volumeText = (TextView) findViewById(R.id.volumeLabel);
+
         mediaPlayer = MediaPlayer.create(this, R.raw.motorcycle);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        final int MAXVOLUME = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+
+
+
+        final SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+        volumeControl.setMax(MAXVOLUME);
+        volumeControl.setProgress(curVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                volumeText.setText(String.valueOf(progress));
+                Log.i("SeekBar value: ", String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     public void playAudio(View view) {
