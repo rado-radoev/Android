b089854228c0efc0a8d83c7d5bf3dcf86386a834
Radoslav Radoev <rradoev@gmail.com>
2018-06-10 23:32:03 -0700

    English - French phrases

--- GridLayout/app/src/main/java/com/superlamer/gridlayout/MainActivity.java
+++ GridLayout/app/src/main/java/com/superlamer/gridlayout/MainActivity.java
@@ -1,7 +1,11 @@
 package com.superlamer.gridlayout;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceID = getResources().getIdentifier(ourId, "raw", "com.superlamer.gridlayout");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceID);
+        mplayer.start();
+
+        Log.i("button tapped", ourId);
     }
 }
