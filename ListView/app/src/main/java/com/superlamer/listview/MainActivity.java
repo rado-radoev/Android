1fe112899b10eb0dcd87175f188f25d467a42ac9
Radoslav Radoev <rradoev@gmail.com>
2018-06-11 17:21:08 -0700

    ListView

---
+++ ListView/app/src/main/java/com/superlamer/listview/MainActivity.java
@@ -0,0 +1,43 @@
+package com.superlamer.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        final ArrayList<String> myFamily = new ArrayList<String>();
+
+        myFamily.add("Rob");
+        myFamily.add("Kristen");
+        myFamily.add("Tommy");
+        myFamily.add("Ralphie");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Person tapped: " , myFamily.get(position));
+                Toast.makeText(getApplicationContext(), "Hello " + myFamily.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
