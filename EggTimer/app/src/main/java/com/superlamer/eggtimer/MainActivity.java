6b565ae4660715d00234db4152d14a459c01e19f
Radoslav Radoev <rradoev@gmail.com>
2018-06-14 00:37:42 -0700

    Reverse the logic in my button OnClick method

--- EggTimer/app/src/main/java/com/superlamer/eggtimer/MainActivity.java
+++ EggTimer/app/src/main/java/com/superlamer/eggtimer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.superlamer.eggtimer;
 
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,11 +18,16 @@ public class MainActivity extends AppCompatActivity {
     private SeekBar timeSeekBar;
     private Button goBtn;
     private boolean isCountDownRunning;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (handler == null) {
+            handler = new Handler();
+        }
 
         goBtn = findViewById(R.id.goBtn);
 
@@ -39,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
                 int minutes = (progress % 3600) / 60;
                 int seconds = (progress % 3600) % 60;
-                Log.i("Minutes: ", String.valueOf(minutes));
-                Log.i("Seconds: ", String.valueOf(seconds));
+//                Log.i("Minutes: ", String.valueOf(minutes));
+//                Log.i("Seconds: ", String.valueOf(seconds));
                 timeDisplay.setText(String.format(Locale.US,"%d:%02d", minutes, seconds));
             }
 
@@ -57,16 +63,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public Runnable timerUpdater = new Runnable() {
+        @Override
+        public void run() {
+            int minutes = Integer.valueOf(timeDisplay.getText().toString().split(":")[0]);
+            int seconds = Integer.valueOf(timeDisplay.getText().toString().split(":")[1]);
+
+            if (minutes > 0 && seconds > 00) {
+                timeDisplay.setText(String.format(Locale.US, "%d:%02d", --minutes, --seconds));
+                handler.postDelayed(this, 1000);
+            }
+            else {
+                handler.removeCallbacks(this);
+            }
+
+
+        }
+    };
+
 
     public void startCountDown(View view) {
 
-        if (isCountDownRunning) {
+        if (!isCountDownRunning) {
             goBtn.setText("GO!");
             timeSeekBar.setVisibility(View.VISIBLE);
+            handler.removeCallbacks(timerUpdater);
+
         }
         else {
             timeSeekBar.setVisibility(View.INVISIBLE);
             goBtn.setText("STOP!");
+            handler.post(timerUpdater);
         }
 
 //        new CountDownTimer(time , 1000) {
