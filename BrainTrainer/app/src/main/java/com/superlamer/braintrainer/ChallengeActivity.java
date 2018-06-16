60eeeb2e62bf9f1b7573df54c66762d4870b1918
Radoslav Radoev <rradoev@gmail.com>
2018-06-16 00:13:30 -0700

    Brain trainer is work in progress. Trying to generate random answers for the buttosn. So far app is crashing

--- BrainTrainer/app/src/main/java/com/superlamer/braintrainer/ChallengeActivity.java
+++ BrainTrainer/app/src/main/java/com/superlamer/braintrainer/ChallengeActivity.java
@@ -1,43 +1,144 @@
 package com.superlamer.braintrainer;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.TextView;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ChallengeActivity extends AppCompatActivity {
 
     private TextView textView;
     private final int COUNTDOWNTIME = 30;
     private TextView countDownText;
+    private TextView answerText;
+    private TextView opText;
+    private int score;
+    private SecureRandom rnd;
+    private final int RANDOM_SEED = 50;
+    private ArrayList<String> operators = new ArrayList<String>();
+    private int activity_challenge;
+    private int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge);
 
+
+        operators = new ArrayList<String>();
+
+        rnd = new SecureRandom();
+
         textView = findViewById(R.id.answerText);
         textView.setVisibility(View.INVISIBLE);
 
+        countDownText = findViewById(R.id.countDownText);
+        answerText = findViewById(R.id.answerText);
+        opText = findViewById(R.id.operationText);
+
+        generateListOfOperators();
+        generateTask();
+        updateButtonText();
         startCountDown();
+    }
+
+    private void generateListOfOperators() {
+        operators.add("+");
+        operators.add("-");
+        operators.add("*");
+        operators.add("/");
+    }
+
+    public void generateTask() {
+        int num1 = rnd.nextInt(10);
+        int num2 = rnd.nextInt(10);
+
+        String operator = operators.get(rnd.nextInt(operators.size()));
+
+        opText.setText(String.format("%d %s %d", num1, operator, num2));
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case  "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+            case "/":
+                if (num1 < num2){
+                    while (num1 > num2) {
+                        num1 = rnd.nextInt(50);
+                    }
+                }
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    private void updateButtonText() {
+        GridLayout gLayout = (GridLayout) findViewById(R.id.answerGrid);
+        int cols = gLayout.getColumnCount();
+        int rows = gLayout.getRowCount();
+        int totalBtns = cols + rows;
+
+        int correctResultBtn = rnd.nextInt(totalBtns);
+
+        ArrayList<View> allButtons;
+        allButtons = ((GridLayout) findViewById(R.id.answerGrid)).getTouchables();
+
+        for (View btn : allButtons) {
+            Button currBtn = (Button) btn;
+            if (currBtn.getTag().equals(String.valueOf(correctResultBtn))) {
+                currBtn.setText(String.valueOf(result));
+            }
+            currBtn.setText(rnd.nextInt(10));
+        }
+
+    }
+
+    public boolean isAnswerCorrect(int userAnser, int expectedAnswer) {
+        return userAnser == expectedAnswer;
     }
 
     public void startCountDown() {
 
-        int countDownTime = Integer.valueOf(countDownText.getText().toString().substring(0,2));
+        //countDownText.setText(String.format("%2ds", COUNTDOWNTIME));
+        final int countDownTime = Integer.valueOf(countDownText.getText().toString().substring(0,2));
 
-        new CountDownTimer(COUNTDOWNTIME, 1000) {
+        countDownText.setTextColor(Color.BLACK);
+        answerText.setAlpha(0);
+
+        new CountDownTimer(COUNTDOWNTIME * 1000, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
-                int time = (int) millisUntilFinished / 60;
+                int time = (int) millisUntilFinished / 1000;
+                Log.i("Time is up", String.valueOf(time));
                 countDownText.setText(String.format("%2ds", time));
             }
 
             @Override
             public void onFinish() {
+                countDownText.setText(String.format("%2ds", 0));
+                countDownText.setTextColor(Color.RED);
+                answerText.setText(String.format("You score is: ", score ));
+                answerText.setAlpha(1);
                 Log.i("Time is up", "");
 
             }
