9eb3f20ae5deac953ccdf21569c62057afd3647c
Radoslav Radoev <rradoev@gmail.com>
2018-08-30 22:59:17 -0700

    Game -wip

--- Connect3_2/app/src/main/java/com/superlamer/connect3_2/MainActivity.java
+++ Connect3_2/app/src/main/java/com/superlamer/connect3_2/MainActivity.java
@@ -3,14 +3,19 @@ package com.superlamer.connect3_2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    // 0 = yellow, 1 = red
+    // 1= yellow, 0 = red
 
     int activePlayer = 0;
+
+    boolean gameIsActive = true;
 
     // 2 means unplayed
 
@@ -23,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(counter.getTag().toString());
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tappedCounter] == 2) {
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
             counter.setTranslationY(-1000f);
 
             if (activePlayer == 0) {
@@ -44,10 +49,43 @@ public class MainActivity extends AppCompatActivity {
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
                         gameState[winningPosition[0]] != 2) {
                     System.out.println(gameState[winningPosition[0]]);
+
+
+                    gameIsActive = false;
+                    String winner = "Yellow";
+
+                    if (gameState[winningPosition[0]] == 0 ) {
+                        winner = "Red";
+                    }
+
+
+                    TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+                    winnerMessage.setText(winner + " has won!");
+
+                    // Someone has won
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
+
                 }
             }
         }
+    }
 
+    public void playAgain(View view) {
+        gameIsActive = true;
+        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+
+        activePlayer = 0;
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayot);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+        }
 
     }
 
