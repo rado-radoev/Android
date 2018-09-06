4345be7b26d2b5e3b726d6183b07c72a78c411f6
Radoslav Radoev <rradoev@gmail.com>
2018-09-05 23:56:22 -0700

    brain trainer - wip

--- BrainTrainer2/app/src/main/java/com/superlamer/braintrainer2/GameActivity.java
+++ BrainTrainer2/app/src/main/java/com/superlamer/braintrainer2/GameActivity.java
@@ -1,8 +1,10 @@
 package com.superlamer.braintrainer2;
 
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,14 +13,28 @@ import java.security.SecureRandom;
 public class GameActivity extends AppCompatActivity {
 
     private TextView countDownTextView;
+    private TextView taskTextView;
+    private TextView answerCorrectness;
+    private TextView scoreTextView;
     private Button playAgain;
     private SecureRandom random;
     private static String[] MATHEMATICAL_OPERATORS;
+    private static int result;
+    private final int MAX_QUESTIONS = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Instantiate score text view
+        scoreTextView = (TextView) findViewById(R.id.scoreTextView);
+
+        // Instantiate answer correctness text field
+        answerCorrectness = (TextView)  findViewById(R.id.answerCorectness);
+
+        // Instantiate taskTextView
+        taskTextView = (TextView) findViewById(R.id.taskTextView);
 
         // Instantiata MATHEMATICAL_OPERATORS array
         MATHEMATICAL_OPERATORS = new String[]{"+" , "-", "/", "*"};
@@ -32,6 +48,12 @@ public class GameActivity extends AppCompatActivity {
         playAgain.setEnabled(false);
 
 
+        startCountDown();
+        updateAnswerFields();
+
+    }
+
+    private void startCountDown() {
         // Start countdown timer. 30 seconds and counting
         countDownTextView = (TextView) findViewById(R.id.countDownTextView);
         countDownTextView.setText("30s");
@@ -58,19 +80,106 @@ public class GameActivity extends AppCompatActivity {
      * Method to generate random math problems.
      * Operations will be addition, substraction, multiplication and division of integers
      * Math problem label will be updated within this method
-     * @return returns integer, the result of the math problem
      */
-    private int generateMathProblem() {
+    private void generateMathProblem() {
         // Grab random operator
+        int MAX = 100;
+
         // generat two random numbers
-        // do the operation
+        int num1 = random.nextInt(MAX) + 1;
+        int num2 = random.nextInt(MAX) + 1;
+        int res = 0;
+
+        // get random operation
+        String operator = MATHEMATICAL_OPERATORS[random.nextInt(MATHEMATICAL_OPERATORS.length)];
+
         // update the text view
-        // output the result
+        taskTextView.setText(String.format("%d %s %d", num1, operator, num2));
+
+        // do the operation
+        switch (operator) {
+            case "+":
+                res = num1 + num2;
+                break;
+            case "-":
+                res = num1 - num2;
+                break;
+            case "*":
+                res = num1 * num2;
+                break;
+            case "/":
+                res = num1 / num2;
+                break;
+            default:
+                res = 0;
+        }
+
+        // output the res
+        result = res;
     }
 
+    /**
+     * Method to generate random numbers and assign to buttons
+     */
     private void updateAnswerFields() {
+
         // add all buttons to array
-        // generate random number for 3 of the buttons
-        // one random button will hold the result
+        Button[] buttons = new Button[] {findViewById(R.id.button0),
+                                        findViewById(R.id.button1),
+                                        findViewById(R.id.button2),
+                                        findViewById(R.id.button3)};
+
+        // generate random number for all of the buttons
+        for (Button b : buttons) {
+            b.setText(random.nextInt(result + 32 - random.nextInt(30)));
+        }
+
+        // Assign reult to a random button
+        buttons[random.nextInt(buttons.length)].setText(result);
+    }
+
+    private boolean isCorrect(int answer) {
+
+        boolean correct = false;
+
+        if (result == answer) {
+            correct = true;
+        }
+
+        return correct;
+    }
+
+    private void updateAnswerCorectness(boolean answerCorrect) {
+
+        answerCorrectness.setVisibility(View.VISIBLE);
+
+        if (answerCorrect) {
+            answerCorrectness.setText("CORRECT");
+            answerCorrectness.setTextColor(Color.rgb(0,255,0));
+        } else {
+            answerCorrectness.setText("WRONG");
+            answerCorrectness.setTextColor(Color.rgb(255,0,0));
+        }
+    }
+
+    private int getUserAnswer() {
+        
+    }
+
+    private void updateScore() {
+        scoreTextView.setText(String.format("%d/%d", 0, MAX_QUESTIONS));
+
+        // if answer is correct update the score
+        if (isCorrect())
+    }
+
+    public void resetGame() {
+        // reset timer to 30s
+        // reset results
+        // reset buttons
+        // hide correct/wrong text
+        // generate new task
+        // disable play again button
+
     }
  }
