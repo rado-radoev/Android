0b87fa6a9cd7ceb7416efb3488c57039737801ac
Radoslav Radoev <rradoev@gmail.com>
2018-09-09 23:53:15 -0700

    Downlaod image

--- DownlaodImage/app/src/main/java/com/superlamer/downlaodimage/MainActivity.java
+++ DownlaodImage/app/src/main/java/com/superlamer/downlaodimage/MainActivity.java
@@ -1,13 +1,74 @@
 package com.superlamer.downlaodimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView dowloadedImg;
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        public Bitmap doInBackground(String... params) {
+
+            Bitmap bitmap = null;
+
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                bitmap = BitmapFactory.decodeStream(inputStream);
+
+
+            } catch (MalformedURLException mfe) {
+                mfe.printStackTrace();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+            return bitmap;
+        }
+    }
+
+    public void downloadImage(View view) {
+
+        // https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png
+
+        ImageDownloader task = new ImageDownloader();
+        Bitmap bitmap;
+
+        try {
+            bitmap = task.execute("https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png").get();
+            dowloadedImg.setImageBitmap(bitmap);
+            Log.i("Interaction ", "Button Tapped");
+        } catch (InterruptedException | ExecutionException ex) {
+            ex.printStackTrace();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dowloadedImg = (ImageView) findViewById(R.id.image);
     }
 }
