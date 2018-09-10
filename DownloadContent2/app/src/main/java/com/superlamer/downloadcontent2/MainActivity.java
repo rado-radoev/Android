8b8841dc99b9a03ee9351ec32405174dab30cbcd
Radoslav Radoev <rradoev@gmail.com>
2018-09-09 22:45:13 -0700

    Download content 2

--- DownloadContent2/app/src/main/java/com/superlamer/downloadcontent2/MainActivity.java
+++ DownloadContent2/app/src/main/java/com/superlamer/downloadcontent2/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +16,36 @@ public class MainActivity extends AppCompatActivity {
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
-        protected String doInBackground(String... strings) {
-            Log.i("URL:" , strings[0]);
-            return "Done";
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                return "Failed";
+            }
         }
     }
 
@@ -26,13 +57,13 @@ public class MainActivity extends AppCompatActivity {
         DownloadTask downloadTask = new DownloadTask();
         String result = null;
         try {
-            downloadTask.execute("http://ecowebhosting.co.uk/").get();
+            result = downloadTask.execute("http://ecowebhosting.co.uk/").get();
         } catch (InterruptedException ie) {
             ie.printStackTrace();
         } catch (ExecutionException ee) {
             ee.printStackTrace();
         }
 
-        Log.i("Result", result)
+        Log.i("Result", result);
     }
 }
