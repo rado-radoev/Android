03693b30d5ff78a8a8df4eb123925048c21b6286
Radoslav Radoev <rradoev@gmail.com>
2018-09-10 00:50:30 -0700

    guess the celebrity - gui created

--- GuessTheCelebrity/app/src/main/java/com/superlamer/guessthecelebrity/MainActivity.java
+++ GuessTheCelebrity/app/src/main/java/com/superlamer/guessthecelebrity/MainActivity.java
@@ -1,13 +1,33 @@
 package com.superlamer.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    String[] names;
+    Bitmap[] celebrityImages;
+
+    public class URLParser extends AsyncTask<String, Void, String> {
+
+        @Override
+        public String doInBackground(String... urls) {
+
+            return null;
+        }
+    }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
