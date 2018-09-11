76fa29e94ec8331e9a7a07e855bfbb81f21fd9e9
Radoslav Radoev <rradoev@gmail.com>
2018-09-11 00:46:51 -0700

    Needs some tweaking

--- CelebrityFaceOff/app/src/main/java/com/superlamer/celebrityfaceoff/MainActivity.java
+++ CelebrityFaceOff/app/src/main/java/com/superlamer/celebrityfaceoff/MainActivity.java
@@ -8,7 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     int choseCeleb = 0;
     int locationOfCorrectAnswer = 0;
     int[] answers = new int[4];
+    Random random;
 
 
     ImageView celebrityImage;
@@ -92,10 +95,69 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    
+    private void generateRandomAnswers() {
+        Button[] buttons = new Button[] { findViewById(R.id.button),
+                                            findViewById(R.id.button2),
+                                            findViewById(R.id.button3),
+                                            findViewById(R.id.button4)};
+
+        int randomCelebIndex = random.nextInt(celebNames.size());
+        int[] usedIndexes = new int[buttons.length];
+        for (int i = 0; i < usedIndexes.length; i++) {
+            usedIndexes[i] = 0;
+        }
+        int count = 0;
+
+        for (Button b : buttons) {
+
+            for (int i = 0; i < usedIndexes.length; i++) {
+                if (usedIndexes[i] == randomCelebIndex) {
+                    randomCelebIndex = random.nextInt(celebNames.size());
+                } else {
+                    usedIndexes[i] = randomCelebIndex;
+                }
+            }
+
+            b.setText(celebNames.get(randomCelebIndex));
+            count++;
+        }
+
+
+
+        int randomButton = random.nextInt(buttons.length - 1);
+        Log.i("Random button", String.valueOf(randomButton));
+        buttons[randomButton].setText(celebNames.get(choseCeleb));
+        Log.i("Chosen celeb", String.valueOf(choseCeleb));
+    }
 
     public void celebChosen(View view) {
+        Button button = (Button) view;
 
+        if (button.getText().equals(celebNames.get(choseCeleb))) {
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Wrong! This is: " + celebNames.get(choseCeleb), Toast.LENGTH_LONG).show();
+        }
+
+        nextQuestion();
+    }
+
+
+    private void nextQuestion() {
+        choseCeleb = random.nextInt(celebNames.size());
+        Log.i("Chosen celeb", String.valueOf(choseCeleb));
+
+        ImageDownloader imageTask = new ImageDownloader();
+        Bitmap celebImage = null;
+        try {
+            celebImage = imageTask.execute(celebURLs.get(choseCeleb)).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        Log.i("Chose celeb URL", celebURLs.get(choseCeleb));
+        celebrityImage.setImageBitmap(celebImage);
+
+        generateRandomAnswers();
     }
 
     @Override
@@ -103,6 +165,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        random = new Random();
         celebrityImage = (ImageView) findViewById(R.id.celebrityImage);
 
         DownloadTask task = new DownloadTask();
@@ -114,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
 
             String[] splitResult = result.split("<div class=\"sidebarContainer\">");
 
-            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Pattern p = Pattern.compile("<img src=\"(.*?)\"");
             Matcher m = p.matcher(splitResult[0]);
 
             while (m.find()) {
@@ -132,13 +195,16 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-            Random random = new Random();
             choseCeleb = random.nextInt(celebNames.size());
+            Log.i("Chosen celeb", String.valueOf(choseCeleb));
 
             ImageDownloader imageTask = new ImageDownloader();
             Bitmap celebImage = null;
             celebImage = imageTask.execute(celebURLs.get(choseCeleb)).get();
+            Log.i("Chose celeb URL", celebURLs.get(choseCeleb));
             celebrityImage.setImageBitmap(celebImage);
+
+            generateRandomAnswers();
 
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
