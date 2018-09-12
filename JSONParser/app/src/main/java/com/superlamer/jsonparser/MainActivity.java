3ca0ca4f71034e793c55216351dafff73bda6d79
Radoslav Radoev <rradoev@gmail.com>
2018-09-11 22:49:43 -0700

    json parser

---
+++ JSONParser/app/src/main/java/com/superlamer/jsonparser/MainActivity.java
@@ -0,0 +1,71 @@
+package com.superlamer.jsonparser;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        public String doInBackground(String... urls) {
+            String result = null;
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+            } catch (JSONException jse) {
+                jse.printStackTrace();
+            }
+
+
+            Log.i("JSON", s);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask downloadTask = new DownloadTask();
+        downloadTask.execute("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+    }
+}
