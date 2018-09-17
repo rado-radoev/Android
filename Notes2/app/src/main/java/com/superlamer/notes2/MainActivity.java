4c40949e94ecb3639d6aea8b9baea4814738664b
Radoslav Radoev <rradoev@gmail.com>
2018-09-17 14:26:42 -0700

    this is f*ed up.

--- Notes2/app/src/main/java/com/superlamer/notes2/MainActivity.java
+++ Notes2/app/src/main/java/com/superlamer/notes2/MainActivity.java
@@ -1,32 +1,107 @@
 package com.superlamer.notes2;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> notes;
-    ArrayAdapter<String> notesAdapter;
+    static ArrayList<String> notes;
+    static ArrayAdapter<String> notesAdapter;
     ListView notesList;
-    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         notes = new ArrayList<>();
+
         notesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
         notesList = (ListView) findViewById(R.id.notesList);
-        sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
 
-        notes.add("Example note");
+        notesList.setAdapter(notesAdapter);
 
+        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("set", null);
+
+        if (set == null) {
+            notes.add("Example note");
+            notesAdapter.notifyDataSetChanged();
+        } else {
+            notes = new ArrayList<>(set);
+            notesAdapter.notifyDataSetChanged();
+        }
+
+        notesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NoteActivity.class);
+                intent.putExtra("noteId", position);
+                startActivity(intent);
+            }
+        });
+
+        notesList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Are  you sure you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                notes.remove(position);
+                                notesAdapter.notifyDataSetChanged();
+
+                                SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+
+                                HashSet<String> set = new HashSet<>(MainActivity.notes);
+
+                                sharedPreferences.edit().putStringSet("set", set).apply();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = this.getMenuInflater();
+        menuInflater.inflate(R.menu.add_new_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.addNewNote) {
+            Intent intent = new Intent(getApplicationContext(), NoteActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- Notes2/app/src/main/java/com/superlamer/notes2/NoteActivity.java
+++ Notes2/app/src/main/java/com/superlamer/notes2/NoteActivity.java
@@ -1,13 +1,62 @@
 package com.superlamer.notes2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.HashSet;
 
 public class NoteActivity extends AppCompatActivity {
+
+    EditText note;
+    int noteId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        note = (EditText) findViewById(R.id.noteEditText);
+
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId", -1);
+
+        if (noteId != -1) {
+            note.setText(MainActivity.notes.get(noteId));
+        } else {
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() -1;
+            MainActivity.notesAdapter.notifyDataSetChanged();
+        }
+
+        note.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId, s.toString());
+                MainActivity.notesAdapter.notifyDataSetChanged();
+                SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+
+                sharedPreferences.edit().putStringSet("set", set).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
