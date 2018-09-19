99921eefaffad8f21d8b7f0b0101eea8261dea0d
Radoslav Radoev <rradoev@gmail.com>
2018-09-19 00:46:00 -0700

    SQL Queries

--- DatabaseDemo/app/src/main/java/com/superlamer/databasedemo/MainActivity.java
+++ DatabaseDemo/app/src/main/java/com/superlamer/databasedemo/MainActivity.java
@@ -14,51 +14,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
+
         try {
             SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
-            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3 ))");
-            sqLiteDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rado', 34)");
-            sqLiteDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Tomy', 4)");
+//            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3 ))");
+            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS newUsers (name VARCHAR, age INT(3), id INTEGER PRIMARY KEY)");
+            sqLiteDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Rado', 34)");
+            sqLiteDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Tomy', 4)");
+            sqLiteDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Victor', 1)");
+            sqLiteDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Patricia', 21)");
 
-            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM users", null);
+//            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM users WHERE age < 18", null);
+
+//            sqLiteDatabase.execSQL("DELETE FROM newUsers WHERE name = 'Victor' LIMIT 1");
+//            sqLiteDatabase.execSQL("UPDATE newUsers SET age = 2 WHERE name = 'Rado'");
+            sqLiteDatabase.execSQL("DELETE FROM newUsers WHERE id = 15");
+
+            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM newUsers WHERE name = 'Victor'", null);
             int nameIndex = c.getColumnIndex("name");
             int ageIndex = c.getColumnIndex("age");
+            int idIndex = c.getColumnIndex("id");
 
             c.moveToFirst();
 
-            while (c != null) {
+            while (c.moveToNext()) {
                 Log.i("name", c.getString(nameIndex));
                 Log.i("age", String.valueOf(c.getInt(ageIndex)));
-
-                c.moveToNext();
+                Log.i("id", String.valueOf(c.getInt(idIndex)));
             }
+
+            c.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        */
-
-        try {
-            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("events", MODE_PRIVATE, null);
-            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
-            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('Bulgaria estblished', 681)");
-            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('Bulgaria liberations', 1878)");
-
-            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM events", null);
-            int nameIndex = c.getColumnIndex("name");
-            int yearIndex = c.getColumnIndex("year");
-
-            while (c != null) {
-
-                Log.i("Event", c.getString(nameIndex));
-                Log.i("Year", String.valueOf(c.getInt(yearIndex)));
 
 
-                c.moveToNext();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("events", MODE_PRIVATE, null);
+//            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
+//            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('Bulgaria estblished', 681)");
+//            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('Bulgaria liberations', 1878)");
+//
+//            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM events", null);
+//            int nameIndex = c.getColumnIndex("name");
+//            int yearIndex = c.getColumnIndex("year");
+//
+//            while (c != null) {
+//
+//                Log.i("Event", c.getString(nameIndex));
+//                Log.i("Year", String.valueOf(c.getInt(yearIndex)));
+//
+//
+//                c.moveToNext();
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
