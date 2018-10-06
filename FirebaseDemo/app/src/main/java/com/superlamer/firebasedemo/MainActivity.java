6e1416d11cd4bbad4f8aa6384532dc7fc765bfab
Radoslav Radoev <rradoev@gmail.com>
2018-10-06 00:45:09 -0700

    firebase and adding adds

---
+++ FirebaseDemo/app/src/main/java/com/superlamer/firebasedemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.superlamer.firebasedemo;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Advanceable;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+/*        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
+        Map<String, String> values = new HashMap<>();
+
+        values.put("wish", "wonna be coder");
+
+        dbref.push().setValue(values, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    Log.i("Info", "Save successful");
+                } else {
+                    Log.i("Info", "Save not successful");
+                }
+
+            }
+        });*/
+
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713.");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+    }
+}
+
